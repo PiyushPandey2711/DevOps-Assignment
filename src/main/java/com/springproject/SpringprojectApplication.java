ddbfe4a442d2e4d1b720b1dd352860da66204b91
piyushpandey <piyushpandey27112000@gmail.com>
2024-03-28 16:15:23 +0530

    first commit

---
+++ src/main/java/com/springproject/SpringprojectApplication.java
@@ -0,0 +1,21 @@
+package com.springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprojectApplication.class, args);
+	}
+	
+	@GetMapping("/message")
+	public String message() {
+		return "this is demo spring project";
+	}
+
+}
